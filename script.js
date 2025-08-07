00191bd1a3a619231d6487398be181f8e3bfcae1
Daniel George <danielgeorgewadea@gmail.com>
2025-08-07 22:56:16 +0300

    Changed the light mode background to be the same as the main website background. Changed the text to be black in light mode.

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ let isDarkMode = false;
 function darkModeToggle() {
     if (isDarkMode) {
         isDarkMode = !isDarkMode;
-        document.body.style.background = "linear-gradient(45deg, #f0f0f0, #d3d3d3)";
+        document.body.style.background = "linear-gradient(to bottom left, #D1E0D7, #607EBC)";
         document.body.style.color = "#1a1a1a";
         document.getElementById("dark-mode").innerHTML = "Light mode";
         document.getElementById("dark-mode").style.color = "#1a1a1a";
         document.getElementById("dark-mode").style.background = "#f0f0f0";
     } else {
         isDarkMode = !isDarkMode;
-        document.body.style.background = "linear-gradient(45deg, #1a1a1a, #333333)";
+        document.body.style.background = "linear-gradient(to bottom left, #1a1a1a, #333333)";
         document.body.style.color = "#f0f0f0";
         document.getElementById("dark-mode").innerHTML = "Dark mode";
         document.getElementById("dark-mode").style.color = "#f0f0f0";
