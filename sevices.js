14b643b97b93e90400197090073f906e8364403a
karim ahmed abd elkareem <karim.1024112@stemoctober.moe.edu.eg>
2025-08-10 17:01:48 +0300

    Add files via upload

--- sevices.js
+++ sevices.js
@@ -1,16 +1,48 @@
-let n = 'no1'
-let now = 'now'
-let ntnow = 'nt-now'
+    setTimeout(() => {
+        console.log("Hiding loader now...");
+        document.getElementById("loader").style.display = "none"
+    }, 3000);
+let n = "no1"
+let now = "now"
+let ntnow = "nt-now"
+// these variables are for the change in the hero section to have the class names and the id
 function change(path, nd){
-    document.getElementById("mod").setAttribute('src', path);
+    document.getElementById("mod").setAttribute("src", path);
+    // here i change the src attribute in the model viewer function to make the model change based on the click button
     document.getElementById(n).classList.remove(now);
+    // here i removed the color of the item that was selected
     document.getElementById(n).classList.add(ntnow);
+    // and gave it the basic class
     n = nd;
     document.getElementById(nd).classList.remove(ntnow);
+    // here i removed the basic class
     document.getElementById(nd).classList.add(now);
+    // here i added the special classs
     transition
 }
+// the small menu function
+document.getElementById('sm-m').addEventListener('click', function() {
+    document.getElementById('ul').classList.toggle('show');
+});
 
+// the quote generator 
+
+let quote = document.getElementById("quote");
+let author = document.getElementById("author");
+const url = "https://quoteslate.vercel.app/api/quotes/random";
+
+let getQuote = () => {
+    fetch(url)
+        .then((data) => data.json())
+        .then((item) => {
+            quote.innerText = "“" + item.quote + "” - ";
+            author.innerText = item.author;
+        });
+};
+
+window.addEventListener("load", getQuote);
+
+// this is the dark mode function of the website
 document.getElementById("dark-mode").addEventListener("click", darkModeToggle);
 
 let isDarkMode = false;
@@ -34,3 +66,4 @@ function darkModeToggle() {
     }
     document.getElementById("website-name").style.color = "white";
 }
+
